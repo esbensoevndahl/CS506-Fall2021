20f3c4cf25bef19079dc28993ed81a76cc8ef8ac
esbensoevndahl <72149499+esbensoevndahl@users.noreply.github.com>
2021-09-23 14:30:01 -0400

    Made road and added crossings

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,28 @@
 def draw_road():
+    print("|    ||    |")
+    print("|    ||    |")
+    print("|    ||    |")
+    print("|====||====|")
+    print("|    ||    |")
+    print("|    ||    |")
+
+    for i in range(len(range(5))):
+        spaces = " " * i
+
+        print((spaces + "\    \\\    \ "))
+
+    print((spaces+("|    ||    |")))
+    print((spaces+("|    ||    |")))
+    print((spaces+("|====||====|")))
+    print((spaces+("|    ||    |")))
+    print((spaces+("|    ||    |")))
+
+    for j in range(len(range(5))):
+        spaces_ = " " * (i - j)
+
+        print(((spaces_) + "/    //    / "))
+
     print("road not found")
     return
+
+draw_road()
