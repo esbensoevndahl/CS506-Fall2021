f610b0b57d6fadcddcb5034a43d7990b4ca662cd
esbensoevndahl <72149499+esbensoevndahl@users.noreply.github.com>
2021-09-23 14:19:12 -0400

    Road with two swings

--- 01-python/infrastructure/road.py
+++ 01-python/infrastructure/road.py
@@ -1,3 +1,27 @@
 def draw_road():
-    print("road not found")
+    print("|    ||    |")
+    print("|    ||    |")
+    print("|    ||    |")
+    print("|    ||    |")
+    print("|    ||    |")
+    print("|    ||    |")
+
+    for i in range(len(range(5))):
+        spaces = " " * i
+
+        print((spaces + "\    \\\    \ "))
+
+    print((spaces+("|    ||    |")))
+    print((spaces+("|    ||    |")))
+    print((spaces+("|    ||    |")))
+    print((spaces+("|    ||    |")))
+    print((spaces+("|    ||    |")))
+
+    for j in range(len(range(5))):
+        spaces_ = " " * (i - j)
+
+        print(((spaces_) + "/    //    / "))
+
     return
+
+draw_road()
